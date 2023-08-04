10a00c447c9f830c66ba4982314511fced1d817c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-08-04 22:22:30 +0700

    Return template, fix question id

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,11 +1,22 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 def index(request):
+    
+    # Get the objects that we want to pass, i.e. the Question's
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    str1 = "Hello world from efefer. You'r at the polls index."
-    return HttpResponse(str1 + "<br>" + output)
+    
+    # Get the template
+    template = loader.get_template("polls/index.html") # will be searched under polls/template
+
+    # Create context dict, to be passed to template
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+
+    # Return the response with template, also need to pass request argument
+    return HttpResponse(template.render(context, request))
 
 
 
