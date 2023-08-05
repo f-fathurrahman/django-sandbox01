1e25cd67a464c8a63fc44c26ea6d1e456597dfe9
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-08-05 11:33:32 +0700

    Using generic views

--- myproject/polls/urls.py
+++ myproject/polls/urls.py
@@ -5,16 +5,26 @@ from . import views
 app_name = "polls"
 
 urlpatterns = [
-    #
-    # example: /polls/
-    path("", views.index, name="index"),
-    #
-    # example: /polls/2/
-    path("<int:question_id>/", views.detail, name="detail"),
-    #
-    # example: /polls/2/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    #
-    # example: /polls/2/vote/
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
+
+
+# Before using generic view
+#urlpatterns = [
+#    #
+#    # example: /polls/
+#    path("", views.index, name="index"),
+#    #
+#    # example: /polls/2/
+#    path("<int:question_id>/", views.detail, name="detail"),
+#    #
+#    # example: /polls/2/results/
+#    path("<int:question_id>/results/", views.results, name="results"),
+#    #
+#    # example: /polls/2/vote/
+#    path("<int:question_id>/vote/", views.vote, name="vote"),
+#]
--- myproject/polls/views_v01.py
+++ myproject/polls/views_v01.py
@@ -1,28 +1,33 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.views import generic
 
-from .models import Choice, Question
-
-class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
-
-    def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+from .models import Question, Choice
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "polls/detail.html"
+def index(request):
+
+    # Get the objects that we want to pass, i.e. the Question's
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+
+    # Create context dict, to be passed to template
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+
+    # Return the response with template, also need to pass request argument
+    return render(request, "polls/index.html", context)
 
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "polls/results.html"
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question" : question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
