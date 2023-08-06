e104b973ab96570320b64e5ca58a6f2d7be44135
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-08-06 18:22:27 +0700

    Add search_fields

--- myproject/polls/admin.py
+++ myproject/polls/admin.py
@@ -22,6 +22,7 @@ class QuestionAdmin(admin.ModelAdmin):
         "question_text", "pub_date", "was_published_recently"
     ]
     list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
